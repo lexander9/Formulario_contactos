649d35ea4f8b85ab702dc243725015a8177b3e81
alexander <alexander.morenoc@outlook.com>
2020-01-23 01:10:05 -0500

    ya esta programado el envio del correo

---
+++ enviar.php
@@ -0,0 +1,21 @@
+<?php
+    //llamando a los campos
+    $nombre=$_POST['nombre'];//post llama a los name
+    $correo=$_POST['correo'];
+    $telefono=$_POST['telefono'];
+    $mensaje=$_POST['mensaje'];
+
+
+    //Datos para el correo
+    $destinatario="alexander.morenoc@outlook.com";
+    $asumto="Contacto desde la web braille";
+    $carta= "De: $nombre \n";
+    $carta .= "Correo: $correo \n";
+    $carta .="Telefono: $telefono \n";
+    $carta .="Mensaje: $mensaje";
+
+
+    //enviando mensaje
+    mail($destinatario, $asunto, $carta);
+    header('Location:mensaje_envio.html');
+?>
